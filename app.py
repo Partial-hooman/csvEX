13a4a8b84e79762e4f3491720432881060bbc8bd
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-28 15:21:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-a
+import streamlit as st
+uploaded_file = st.file_uploader("Choose a CSV file")
+if uploaded_file is not None:
+   tab1, tab2, tab3, tab4 = st.tabs(["Dataframe", "Numeric Series", "Text Series", "Datetime Series"])
+   with tab1:
+     pass
