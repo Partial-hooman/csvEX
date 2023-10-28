8af8c746533d50182a81d3bf75cd9b50108528fd
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-28 21:26:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ if uploaded_file is not None:
    tab1, tab2, tab3, tab4 = st.tabs(["Dataframe", "Numeric Series", "Text Series", "Datetime Series"])
    with tab1:
      pass
+   with tab2:
+     pass
+   with tab3:
+     pass
+   with tab4:
+     pass
+
