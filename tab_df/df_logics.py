953bb2792c87c7d0697b68e413141bf5ae77fd58
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-29 20:29:08 +0530

    Update df_logics.py

--- tab_df/df_logics.py
+++ tab_df/df_logics.py
@@ -1,1 +1,28 @@
+import pandas
+from datetime import datetime
+from dateutil.parser import parse
 
+
+
+def get_df_information(df):
+    Number_of_Rows = len(df)
+    Number_of_Columns = len(df.columns)
+    Number_of_Duplicated_Rows = len(df) - len(df.drop_duplicates())
+    Number_of_Rows_with_Missing_Values = df.isnull().sum().sum()
+    Columns = []
+    Column_dtype = []
+    column_memory = list(df.memory_usage(index = False))
+    for x in list(df.columns)
+        try:
+         n =  [float(ele) for ele in df[x]]
+         Columns.append(x)
+         Column_dtype.append('numeric')
+        except:
+         try:
+          t = [parse(i) for i in df[x]]
+          Columns.append(x)
+          Column_dtype.append('date')
+         except:
+          Columns.append(x)
+          Column_dtype.append('text')
+     
