20cb82993dd678bb8ba47b76b1e03b6054b68610
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 03:05:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ uploaded_file = st.file_uploader("Choose a CSV file")
 if uploaded_file is not None:
    tab1, tab2, tab3, tab4 = st.tabs(["Dataframe", "Numeric Series", "Text Series", "Datetime Series"])
    with tab1:
-     pass
+     df = pd.read_csv(uploaded_file)
+     df_display(df)
    with tab2:
      pass
    with tab3:
