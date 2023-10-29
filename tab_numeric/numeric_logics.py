a0ff2305842772bc05f63a8270a09189c6d9669d
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 03:42:08 +0530

    Update numeric_logics.py

--- tab_numeric/numeric_logics.py
+++ tab_numeric/numeric_logics.py
@@ -1,7 +1,8 @@
 import pandas
 
 def get_numeric_info(col):
-  uniq_val = len(Counter(input_list).keys())
-  missing_val = 
+  uniq_val = len(Counter(col).keys())
+  missing_val = col.isna().sum()
+  values = 
   for x in col:
-        
+      if x ==   
