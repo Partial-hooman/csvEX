2be0e5b963937237fcd7bfd8f59a37f0279a6e07
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-29 20:49:35 +0530

    Update df_display.py

--- tab_df/df_display.py
+++ tab_df/df_display.py
@@ -1,4 +1,6 @@
 import streamlit
 from df_logics import get_df_information
+import pandas
 
-
+def df_display(df):
+    num_rows, num_columns, no_duplicate_rows, num_missing_val, columns, columns_dtype, column_memory = 
