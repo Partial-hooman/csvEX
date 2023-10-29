c82feb440be66b3d8ca3c77e256816d68a29878a
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-29 21:44:06 +0530

    Update df_display.py

--- tab_df/df_display.py
+++ tab_df/df_display.py
@@ -3,4 +3,12 @@ from df_logics import get_df_information
 import pandas
 
 def df_display(df):
-    num_rows, num_columns, no_duplicate_rows, num_missing_val, columns, columns_dtype, column_memory = 
+    num_rows, num_columns, no_duplicate_rows, num_missing_val, columns, columns_dtype, column_memory = get_df_information(df) 
+    with st.expander('Dataframe'):
+         table = {'Description':['Number of Rows','Number of columns','Number of Duplicate Rows','Number of Rows with Missing Values'],'Value':[num_rows, num_columns, no_duplicate_rows, num_missing_val]}
+         st.table(table)
+    with st.expander('Columns'):
+         table2 = {'column':columns,'Data_type':columns_dtype,'memory':column_memory}
+         st.table(table2)
+    with st.expander('Explore Dataframe'):
+         pass
