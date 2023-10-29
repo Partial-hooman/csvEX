f9699d7114f5990f831898adfc871b9658937633
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 03:54:33 +0530

    Update numeric_logics.py

--- tab_numeric/numeric_logics.py
+++ tab_numeric/numeric_logics.py
@@ -1,8 +1,15 @@
 import pandas
-
+import statistics
 def get_numeric_info(col):
   uniq_val = len(Counter(col).keys())
   missing_val = col.isna().sum()
   values = [float(n) for n in col]
-  for x in col:
-      if x ==    
+  n_zeroes = 0
+  num_neg = 0
+  for x in values:
+      if x == 0:
+        n_zeroes = n_zeroes + 1
+      elif x < 0:
+        num_neg = num_neg + 1
+  avg=sum(values)/len(values)
+  std=statistics.stdev(values)
