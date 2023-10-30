89021dd6748133f2d7b95b29d5599593db08e6eb
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-31 00:02:06 +0530

    Update text_display.py

--- tab_text/text_display.py
+++ tab_text/text_display.py
@@ -10,8 +10,8 @@ def display_text(df):
     text_tble = {'Description':['Number of Unique Values','Number of Rows with Missing Values','Number of Empty Rows','Number of Rows with Only Whitespace','Number of Rows with Only Lowercases','Number of Rows with Only Uppercases','Number of Rows with Only Alphabet','Number of Rows with Only Digits','Mode Value'],'Value':[uniq_val_len, missing_val, empty_rows, whitespace_no, lowercase_no, uppercase_no, alphabet_no, digit_no, mode_val]}
     with st.expander('Text Column'):
          st.table(text_tble)
-    with st.('Bar Chart'):
+    with st.expander('Bar Chart'):
          bar = alt.Chart().mark_bar({'Value':unick_vals, 'occurence':occurences}).encode(x = 'Value',  y = 'occurence')
          st.altair_chart(bar)
-    with st.('Most Frequent Values'):
+    with st.expander('Most Frequent Values'):
          st.table({'Value':unick_vals[0:21], 'occurence':occurences[0:21], 'percentage':percntages[0:21]})
