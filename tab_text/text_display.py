7f08a9c993d391b9ec3252d1887d41c2fa9a6f6b
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 21:12:50 +0530

    Update text_display.py

--- tab_text/text_display.py
+++ tab_text/text_display.py
@@ -2,5 +2,7 @@ import streamlit as st
 from tab_text.text_logics import get_text_info 
 import altair as alt
 
-def display_text():
+def display_text(df):
+    cols = [ col  for col, dt in df.dtypes.items() if dt == object]
+    uniq_val_len, missing_val, empty_rows, whitespace_no, lowercase_no, uppercase_no, alphabet_no, digit_no, mode_val, unick_vals, occurences, percentages = get_text_info()
     
