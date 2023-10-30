c686f921503b0f469d5d12cd77ea93a07b8f19c3
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 21:15:03 +0530

    Update text_display.py

--- tab_text/text_display.py
+++ tab_text/text_display.py
@@ -3,6 +3,7 @@ from tab_text.text_logics import get_text_info
 import altair as alt
 
 def display_text(df):
-    cols = [ col  for col, dt in df.dtypes.items() if dt == object]
-    uniq_val_len, missing_val, empty_rows, whitespace_no, lowercase_no, uppercase_no, alphabet_no, digit_no, mode_val, unick_vals, occurences, percentages = get_text_info()
+    text_cols = [ col  for col, dt in df.dtypes.items() if dt == object]
+    text_Col = st.selectbox('Which text column do you want to explore',text_cols)
+    uniq_val_len, missing_val, empty_rows, whitespace_no, lowercase_no, uppercase_no, alphabet_no, digit_no, mode_val, unick_vals, occurences, percentages = get_text_info(text_Col)
     
