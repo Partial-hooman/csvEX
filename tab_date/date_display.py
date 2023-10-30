a8985cdf87f02d1d3060e7597c2d11f0b47df36a
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 23:24:29 +0530

    Update date_display.py

--- tab_date/date_display.py
+++ tab_date/date_display.py
@@ -18,4 +18,8 @@ def display_date(df):
     dt_table = {'Description':['Number of Unique Values','Number of Rows with Missing Values','Number of Weekend Dates','Number of Weekday Dates','Number of Dates in Future','Number of Rows with 1900-01-01','Number of Rows with 1970-01-01','Minimum Value','Maximum Value'],'Values':[uniq_val_len, missing_val, weekend_date_no, weekday_date_no, future_date_no, rows_with_1900_01_01, rows_with_1970_01_01, minima_val, maxima_val]}
     with st.expander('Date Column'):
         st.table(dt_table)
-    with st.expander('Bar Chart'):
+    with st.expander('Histogram Chart'):
+        hist1 = alt.Chart().mark_bar(dt_table).encode(x = 'Values',  y = 'count()') 
+        st.altair_chart(hist1)
+    with st.expander('Most Frequent Values'):
+        
