61a4778fc378c283a39d2de95d3828349d9783f4
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 21:33:58 +0530

    Update text_display.py

--- tab_text/text_display.py
+++ tab_text/text_display.py
@@ -6,4 +6,8 @@ def display_text(df):
     text_cols = [ col  for col, dt in df.dtypes.items() if dt == object]
     text_Col = st.selectbox('Which text column do you want to explore',text_cols)
     uniq_val_len, missing_val, empty_rows, whitespace_no, lowercase_no, uppercase_no, alphabet_no, digit_no, mode_val, unick_vals, occurences, percentages = get_text_info(text_Col)
-    
+    text_tble = {'Description':['Number of Unique Values','Number of Rows with Missing Values','Number of Empty Rows','Number of Rows with Only Whitespace','Number of Rows with Only Lowercases','Number of Rows with Only Uppercases','Number of Rows with Only Alphabet','Number of Rows with Only Digits','Mode Value'],'Value':[uniq_val_len, missing_val, empty_rows, whitespace_no, lowercase_no, uppercase_no, alphabet_no, digit_no, mode_val]}
+    with st.expander('Text Column'):
+         st.table(text_tble)
+    with st.('Bar Chart'):
+         
