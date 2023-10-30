e2b9cdf557108f649c7356c4bd1adedb7df13f67
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 20:53:54 +0530

    Update text_logics.py

--- tab_text/text_logics.py
+++ tab_text/text_logics.py
@@ -1,4 +1,31 @@
 import pandas as pd
+from statistics import mode
 
 def get_text_column_info(col):
-    
+    uniq_val_len = len(Counter(list(col)).keys())
+    missing_val = col.isna().sum()
+    empty_rows = list(col).count('')
+    whitespace_no = 0
+    lowercase_no = 0
+    uppercase_no = 0
+    alphabet_no = 0
+    digit_no = 0
+    mode_val = mode(list(col))
+    unick_vals = []
+    occurences = []
+    percentages = []
+    for x in col:
+        if x.isspace() == True:
+           whitespace_no += 1
+        elif x.islower() == True:
+            lowercase_no += 1
+        elif x.isupper() == True:
+            uppercase_no += 1
+        elif x.isalpha() == True:
+            alphabet_no += 1
+        elif x.isnumeric() == True:
+            digit_no += 1 
+        if x not in unick_vals:
+           unick_vals.append(x)
+           occurences.append(list(col).count(x))
+           
