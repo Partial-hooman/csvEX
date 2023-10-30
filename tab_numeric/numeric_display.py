a35d128172997c0d8714dbffd3180def55b99b2c
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 23:58:17 +0530

    Update numeric_display.py

--- tab_numeric/numeric_display.py
+++ tab_numeric/numeric_display.py
@@ -15,10 +15,10 @@ def display_numeric(df):
     tble = {'Description':['Number of unique values','Number of Rows with missing values','Number of Rows with 0','Number of Rows with Negative Values','Average Value','Standard Deviation Value','Minimum Value','Median Value'],"Value":[uniq_val, missing_val, n_zeroes, num_neg, avg, std, max_val, min_val, median_val]}
     with st.expander('Numeric Column'):
          st.table(tble)
-     with st.expander('Histogram chart'):
+    with st.expander('Histogram chart'):
          hist = alt.Chart().mark_bar(tble).encode(x = 'Value',  y = 'count()') 
          st.altair_chart(hist)
-     with st.expander('Most Frequent Values'):
+    with st.expander('Most Frequent Values'):
          st.table({'Value':unique_vals[0:21], 'occurence':occurence[0:21], 'percentage':percntage[0:21]})
          
          
