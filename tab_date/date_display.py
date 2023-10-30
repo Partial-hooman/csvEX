3a01c248a052f728040aedf062bcd012e1c63bf7
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 22:58:38 +0530

    Update date_display.py

--- tab_date/date_display.py
+++ tab_date/date_display.py
@@ -2,12 +2,15 @@ import streamlit as st
 from tab_date.date_logics import get_date_info 
 import altair as alt
 import pandas as pd
+from datetime import datetime
+from dateutil.parser import parse
 
 def display_date(df):
     date_cols = []
     for x in list(df.columns):
       try:
-        n = [float(i) for i in df[x]]
-        num_cols.append(x)
+        dt = [parse(d) for d in df[x]]
+        date_cols.append(x)
       except:
         pass
+    date_col = 
