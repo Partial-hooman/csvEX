c53a4a152c7e16650011d5eee47eed0dcc879215
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 22:55:37 +0530

    Update date_display.py

--- tab_date/date_display.py
+++ tab_date/date_display.py
@@ -4,4 +4,10 @@ import altair as alt
 import pandas as pd
 
 def display_date(df):
-    date_cols = 
+    date_cols = []
+    for x in list(df.columns):
+      try:
+        n = [float(i) for i in df[x]]
+        num_cols.append(x)
+      except:
+        pass
