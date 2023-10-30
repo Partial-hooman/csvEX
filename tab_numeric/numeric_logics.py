2df928bf085ee066f067cb439f326e85949354e0
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 19:52:42 +0530

    Update numeric_logics.py

--- tab_numeric/numeric_logics.py
+++ tab_numeric/numeric_logics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import statistics
 def get_numeric_info(col):
-  uniq_val = len(Counter(col).keys())
+  uniq_val_len = len(Counter(list(col)).keys())
   missing_val = col.isna().sum()
   values = [float(n) for n in col]
   n_zeroes = 0
@@ -16,4 +16,12 @@ def get_numeric_info(col):
   max_val=max(values)
   min_val=min(values)
   median_val=statistics.median(values)
-  return uniq_val, missing_val, n_zeroes, num_neg, avg, std, max_val, min_val, median_val
+  unique_vals = Counter(list(col)).keys()
+  occurence = []
+  percntage = []
+  for x in unique_vals:
+      occurence.append(list(col).count(x))
+      percntage.append((list(col).count(x)/len(list(col)))*100)
+      
+  
+  return uniq_val, missing_val, n_zeroes, num_neg, avg, std, max_val, min_val, median_val, unique_vals, occurence, percntage
