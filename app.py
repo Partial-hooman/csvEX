2f9898993da2ed0b88323f1036bd3e32a02c1024
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 23:33:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import pandas as pd
 from tab_df.df_display import df_display
+from tab_numeric.numeric_display import numeric_display
+from tab_text.text_display import text_display
+from tab_date.date_display import date_display
 import streamlit as st
 uploaded_file = st.file_uploader("Choose a CSV file")
 if uploaded_file is not None:
+   numeric_display
    tab1, tab2, tab3, tab4 = st.tabs(["Dataframe", "Numeric Series", "Text Series", "Datetime Series"])
    with tab1:
-     df = pd.read_csv(uploaded_file)
      df_display(df)
    with tab2:
-     pass
+     numeric_display(df)
    with tab3:
-     pass
+     text_display(df)
    with tab4:
-     pass
+     date_display(df)
 
