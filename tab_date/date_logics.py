231ec0e1358c35474dbcb0c89dfccd17c59b2599
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-10-30 22:48:57 +0530

    Update date_logics.py

--- tab_date/date_logics.py
+++ tab_date/date_logics.py
@@ -12,9 +12,9 @@ def get_date_info(col):
     rows_with_1970_01_01 = 0
     minima_val = min(list(col))
     maxima_val = max(list(col))
-    unick_vals = []
-    occurences = []
-    percentages = []
+    unic_vals = []
+    occurencess = []
+    percentagess = []
     for x in col:
         if x.weekday() < 5:
            weekday_date_no += 1
@@ -26,3 +26,9 @@ def get_date_info(col):
            rows_with_1900_01_01 += 1
         if  '1970-01-01' in str(x):
            rows_with_1970_01_01 += 1
+        if x not in unic_vals:
+           unic_vals.append(x)
+           occurencess.append(list(col).count(x))
+           percentagess.append((list(col).count(x)/len(list(col)))*100)
+            
+    return uniq_val_len, missing_val, weekend_date_no, weekday_date_no, future_date_no, rows_with_1900_01_01, rows_with_1970_01_01, minima_val, maxima_val, unic_vals, occurencess, percentagess
